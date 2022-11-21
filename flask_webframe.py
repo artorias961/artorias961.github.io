64e5625568e212277b11962f8571dd148e2d5667
artorias961 <artorias961@gmail.com>
2022-11-21 00:53:17 -0800

    my current files

--- flask_webframe.py
+++ flask_webframe.py
@@ -1,17 +1,22 @@
 from flask import Flask, render_template
+import http.server
+import socketserver
 import requests
 import json
 
 app = Flask(__name__)
 
-@app.route('/home')
-def hello_name(name):
-     return 'Hello ArtyChan T-T'
+
+@app.route('/')
+def hello_name():
+     return render_template('main_page.html')
+
 
 if __name__ == '__main__':
      hello_name()
-     app.run(host="0.0.0.0", port=80)
+     app.run(debug=True, port=8000)
+#    app.run(debug=True, host="0.0.0.0", port=80)
 #    print("To run the program type in the following on the terminal:")
 #    print("flask --app .\local_server_file.py run")
 #    print("\t OR")
-#    print("flask --app .\local_server_file.py --debug run")
+#    print("flask --app .\local_server_file.py --debug run")c
---
+++ main_local_server.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template
+import http.server
+import socketserver
+import sys
+import requests
+import json
+
+def setting_up_server():
+    # Creating the handle of the server
+    handler_class = http.server.SimpleHTTPRequestHandler 
+    
+    # Creating the server
+    server_class = http.server.HTTP
+
+    # Creating the protocol
+    protocol = "HTTP/1.0"
+
+    # Creating the port number
+    if sys.argv[1:]:
+        port = int(sys.argv[1])
+    else:
+        port = 8000
+
+    # Creating the server address
+    server_address = ('localhost', port)
+
+    # Invoking server operations
+    handler_class.protocol_version = protocol
+    http = server_class(server_address, handler_class)
+
+    # Getting logs for the server
+    http_socket_name = http.socket.getsockname()
+    
+    # Printing the current local server address protocol
+    print("Serving HTTP on", http_socket_name[0], "port", http_socket_name[1], '...')
+
+    # Making the server run until terminal command breaks it
+    http.serve_forever()
+
+if __name__ == '__main__':
+    setting_up_server()
+
+
+
+
+# To run the server then type in the terminal:
+# python -m http.server
