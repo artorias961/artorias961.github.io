1851e5acfee009446d4ea3db81ccf717fb189218
artorias961 <artorias961@gmail.com>
2023-01-04 22:30:28 -0800

    Working on a second page

---
+++ local_node.js
@@ -0,0 +1,24 @@
+/*
+* This is an alternative to the python server local host.
+* Just learned this (1/3/2023)
+* Link: https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/Introduction
+*/
+// Load HTTP module
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 8000;
+
+// Create HTTP server
+const server = http.createServer(function (req, res) {
+    // Set the response HTTP header with HTTP status and Content type
+    res.writeHead(200, {"Content-Type": "text/plain"});
+
+    // Send the response body "Hello World"
+    res.end("Hello World\n");
+});
+
+// Prints a log once the server starts listening
+server.listen(port, hostname, function () {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
