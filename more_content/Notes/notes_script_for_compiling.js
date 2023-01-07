df691ce5b4d12b5a74a0aff2d4f2c12e154484be
artorias961 <artorias961@gmail.com>
2023-01-06 22:28:01 -0800

    updated the website:), not working but heading the correct direction

---
+++ more_content/Notes/notes_script_for_compiling.js
@@ -0,0 +1,15 @@
+const sass = require('node-sass');
+const fs = require('fs');
+
+// Read the JSON file
+const options = JSON.parse(fs.readFileSync('run_package.json', 'utf8'));
+
+// Compile the SCSS
+sass.render(options, function(error, result) {
+  if (error) {
+    console.log(error);
+  } else {
+    // Write the CSS to a file
+    fs.writeFileSync(options.outFile, result.css);
+  }
+});
