5bfbd9d901f7b2386bb59e61518fb60d4a5aa3b6
artorias961 <artorias961@gmail.com>
2023-01-06 23:37:11 -0800

    fixing minor stuff

--- more_content/Notes/notes_script_for_compiling.js
+++ more_content/Notes/notes_script_for_compiling.js
@@ -4,12 +4,12 @@ const fs = require('fs');
 // Read the JSON file
 const options = JSON.parse(fs.readFileSync('run_package.json', 'utf8'));
 
-// Compile the SCSS
+// Render the SCSS to CSS
 sass.render(options, function(error, result) {
   if (error) {
     console.log(error);
   } else {
     // Write the CSS to a file
-    fs.writeFileSync(options.outFile, result.css);
+    fs.writeFileSync(options.outFile, styles.css);
   }
 });
