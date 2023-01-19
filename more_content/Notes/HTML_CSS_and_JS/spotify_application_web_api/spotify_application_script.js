d80a4557d495ee55f16fa7bc0a638f80ef83bf6a
artorias961 <artorias961@gmail.com>
2023-01-18 21:15:47 -0800

    More workable links:)

---
+++ more_content/Notes/HTML_CSS_and_JS/spotify_application_web_api/spotify_application_script.js
@@ -0,0 +1,7 @@
+/*
+* Reference Link: https://developer.spotify.com/documentation/web-api/#:~:text=The%20base%20address%20of%20Web,permission%20to%20access%20the%20data.
+* Reference Link: https://developer.spotify.com
+* Reference Link: https://developer.spotify.com/documentation/general/guides/authorization/
+* Reference Link: https://developer.spotify.com/documentation/general/guides/authorization/code-flow/
+*
+*/
