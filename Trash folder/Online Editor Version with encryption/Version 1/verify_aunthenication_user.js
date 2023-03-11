b13100cb642f4461ba7b470d649f4c6f4e0e2d6f
artorias961 <artorias961@gmail.com>
2023-03-11 05:23:35 -0800

    updated the website

--- Trash folder/Online Editor Version with encryption/Version 1/verify_aunthenication_user.js
+++ Trash folder/Online Editor Version with encryption/Version 1/verify_aunthenication_user.js
@@ -1,19 +1,3 @@
-/*
-Checklist for verification (airs lin advice):
-- Restrict IP address (within the country/state)
-- Add a firewall
-- Add a framework server language (for JS, PHP is different)
-- Don't store password or username in ANY JS file's
-- Add server side validation
-- Add SQL injection prevention
-- (Optional) Add a multi-factor authorization
-- Security must require constant updates and monitoring (activity in the server)
-- HIDE ALL ERRORS (to prevent hackers to use the errors agains't the website)
-- Add rate limiting
-- Add login attempts
-- (Optional) Create a white list in the server side language
-*/
-
 // Assuming these are the correct username and password
 const correctUsername = "artorias961";
 const correctPassword = "arty";
@@ -31,12 +15,6 @@ function authenticate() {
     } else {
         alert("Incorrect username or password.");
     }
-
-    // Checking if the user forget to submit one of the boxes
-    if (!name || !password) {
-		res.status(400).send('Please fill in all fields.');
-
-	}
 }
 
 /*
